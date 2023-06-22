8966a1a740e71f981971cd7720d161c29bb590de
ck-siddharth <siddharth.s@codeking.in>
2023-06-22 16:29:33 +0530

    initial commit 2

--- src/Embed.js
+++ src/Embed.js
@@ -1,34 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Embed = ({ dataId }) => {
+  const [headings, setHeadings] = useState([]);
+
   useEffect(() => {
     const data = {
       '001': ['Heading 1', 'Heading 2', 'Heading 3', 'Heading 4', 'Heading 5'],
       '002': ['Title 1', 'Title 2', 'Title 3', 'Title 4', 'Title 5'],
     };
 
-    const headings = data[dataId];
-
-    if (headings) {
-      const ulElement = document.createElement('ul');
-
-      headings.forEach((heading) => {
-        const liElement = document.createElement('li');
-        liElement.textContent = heading;
-        ulElement.appendChild(liElement);
-      });
-
-      const element = document.getElementById('headings');
-
-      while (element.firstChild) {
-        element.firstChild.remove();
-      }
-
-      element.appendChild(ulElement);
-    }
+    setHeadings(data[dataId] || []);
   }, [dataId]);
 
-  return <div id="headings" />;
+  return (
+    <div id="headings">
+      <ul>
+        {headings.map((heading, index) => (
+          <li key={index}>{heading}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Embed;
